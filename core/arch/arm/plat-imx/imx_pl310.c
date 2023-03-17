nxp-imx/imx-optee-os

imx_pl310 add pl310_enabled API
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/arch/arm/plat-imx/imx_pl310.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
@@ -55,6 +56,11 @@ void arm_cl2_config(vaddr_t pl310_base)
 
 	/* invalidate all cache ways */
 	arm_cl2_invbyway(pl310_base);
+}
+
+bool pl310_enabled(vaddr_t pl310_base)
+{
+	return read32(pl310_base + PL310_CTRL) & 1;
 }
 
 void arm_cl2_enable(vaddr_t pl310_base)

core/arch/arm/plat-imx/imx_pl310.h
@@ -7,6 +7,7 @@
 
 uint32_t pl310_enable(void);
 uint32_t pl310_disable(void);
+bool arm_cl2_enabled(vaddr_t pl310_base);
 uint32_t pl310_enable_writeback(void);
 uint32_t pl310_disable_writeback(void);
 uint32_t pl310_enable_wflz(void);

quaresmajose/optee_os

(cherry picked from commit 1265be91b1f844b0d88f6cae809f57827bf3730c)

core/arch/arm/plat-imx/imx_pl310.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
@@ -61,6 +62,11 @@ void arm_cl2_config(vaddr_t pl310_base)
 
 	/* invalidate all cache ways */
 	arm_cl2_invbyway(pl310_base);
+}
+
+bool pl310_enabled(vaddr_t pl310_base)
+{
+	return io_read32(pl310_base + PL310_CTRL) & 1;
 }
 
 void arm_cl2_enable(vaddr_t pl310_base)

core/arch/arm/plat-imx/imx_pl310.h
@@ -7,6 +7,7 @@
 
 uint32_t pl310_enable(void);
 uint32_t pl310_disable(void);
+bool pl310_enabled(vaddr_t pl310_base);
 uint32_t pl310_enable_writeback(void);
 uint32_t pl310_disable_writeback(void);
 uint32_t pl310_enable_wflz(void);