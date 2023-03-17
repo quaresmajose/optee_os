nxp-imx/imx-optee-os

MLK-18036 Add dt_overwrite and dt_find_ocram_tz_addr functions
* dt_overwrite() overwrites specified properties in the device tree.
  Properties to overwrite and new properties are also specified
  in the device tree.

  The format is the following :

  overw_str = <&node_1 prop_1 ... prop_n>,
		<&node_2 prop_1 ... prop_n>,
		...
		<&node_n prop_1 ... prop_n>;
  prop_n variables are 32bit integers.

* dt_find_ocram_tz_addr() defines the beginning the start address of
  the TZ ocram space.
  The OCRAM space allocated to optee is Trustzone protected. Only the
  begin address of the protection is specifiable.
  This function fetches the address of the node to protect in the dtb.
  Nodes to protect are specified by its compatible string in
  tz_ocram_match[] tab.

Signed-off-by: Clement Faure <clement.faure@nxp.com>

core/arch/arm/plat-imx/imx_dt.c
@@ -1,0 +1,146 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018 NXP
+ *
+ */
+#include <trace.h>
+#include <libfdt.h>
+#include <kernel/generic_boot.h>
+#include <imx.h>
+
+#define DT_U32_PROP_SIZE	4
+#define DT_NUM_PROP_MAX		5
+
+/*
+ * dt_overwrite() overwrites specified properties in the device tree.
+ * Properties to overwrite and new properties are also specified in
+ * the device tree.
+ *
+ * The format is the following :
+ *
+ * overw_str = <&node_1 prop_1 ... prop_n>,
+ *		<&node_2 prop_1 ... prop_n>,
+ *		...
+ *		<&node_n prop_1 ... prop_n>;
+ *
+ * prop_n variables are 32bit integers.
+ *
+ * @node_str: Compatible string of optee node.
+ * @prop_str: Property to overwrite (reg, clocks, ...).
+ * @overw_str: Array in node_str containing new values to overwrite property
+ *		specified prop_str.
+ * @size_prop: Size of the property to overwrite.
+ */
+void dt_overwrite(const char *node_str, const char *prop_str,
+				const char *overw_str, uint32_t size_prop)
+{
+	void *fdt, *prop;
+	uint32_t *fdt_overw_node;
+	int offset, phandle, ret, len;
+	uint32_t i, j, sub_node, sub[DT_NUM_PROP_MAX];
+
+	if (size_prop > DT_NUM_PROP_MAX)
+		EMSG("Number of properties to overwrite is too high");
+
+	if (node_str == NULL) {
+		EMSG("Compatible string empty");
+		return;
+	}
+
+	/* Get dtb */
+	fdt = get_dt();
+	if (fdt == NULL) {
+		EMSG("No DTB found");
+		return;
+	}
+
+	/* Get offset of node_str */
+	offset = fdt_node_offset_by_compatible(fdt, 0, node_str);
+	if (offset < 0) {
+		EMSG("Cannot find %s node in the device tree", node_str);
+		return;
+	}
+
+	/* Get overw_node property */
+	fdt_overw_node = (uint32_t *)fdt_getprop(fdt, offset, overw_str, &len);
+	if (!fdt_overw_node) {
+		DMSG("%s not found in %s", overw_str, node_str);
+		return;
+	}
+	sub_node = len / (size_prop * DT_U32_PROP_SIZE);
+
+	/* For each node to substitute */
+	for (i = 0; i < sub_node; i++) {
+
+		/* fdt_overw_node[0] has to be the phandle */
+		phandle = fdt32_to_cpu(fdt_overw_node[i*size_prop]);
+
+		/* Save new properties starting from fdt_overw_node[1] */
+		for (j = 0; j < size_prop; j++)
+			sub[j] = fdt_overw_node[i*size_prop+j+1];
+
+		/* Get offset based on the phandle */
+		offset = fdt_node_offset_by_phandle(fdt, phandle);
+		if (offset < 0) {
+			EMSG("Cannot find offset based on phandle");
+			return;
+		}
+
+		/* Get size of prop_str */
+		prop = fdt_getprop_w(fdt, offset, prop_str, &len);
+		if (!prop) {
+			DMSG("There is no property %s in the phandle %d",
+							prop_str, phandle);
+			return;
+		}
+
+		/* Replace prop_str property */
+		ret = fdt_setprop_inplace(fdt, offset, prop_str, sub, len);
+		if (ret) {
+			EMSG("Error setprop inplace ret=%d", ret);
+			return;
+		}
+	}
+}
+
+void dt_debug(const char *node_str, const char *prop)
+{
+	void *fdt;
+	uint32_t *fdt_prop;
+	int offset;
+
+	if (node_str == NULL) {
+		EMSG("Compatible string empty");
+		return;
+	}
+
+	/* Get dtb */
+	fdt = get_dt();
+	if (fdt == NULL) {
+		EMSG("No DTB found");
+		return;
+	}
+
+	/* Get offset of node_str */
+	offset = fdt_node_offset_by_compatible(fdt, 0, node_str);
+	if (offset < 0) {
+		EMSG("Cannot find %s node in the device tree", node_str);
+		return;
+	}
+
+	/* Get overw_clock property */
+	fdt_prop = (uint32_t *)fdt_getprop(fdt, offset, prop, NULL);
+	if (!fdt_prop) {
+		EMSG("%s not found in %s", prop, node_str);
+		return;
+	}
+
+	DMSG(
+		"Node: %s prop %s = 0x%X, 0x%X, 0x%X",
+			node_str,
+			prop,
+			fdt32_to_cpu(fdt_prop[0]),
+			fdt32_to_cpu(fdt_prop[1]),
+			fdt32_to_cpu(fdt_prop[2])
+		);
+}

core/arch/arm/plat-imx/imx_ocram.c
@@ -9,11 +9,98 @@
 #include <mm/core_memprot.h>
 #include <string.h>
 #include <io.h>
-
 #include <imx.h>
 #include <imx_pm.h>
+#ifdef CFG_DT
+#include <libfdt.h>
+#include <kernel/generic_boot.h>
+#include <kernel/dt.h>
+#endif /* CFG_DT */
 
 paddr_t iram_tlb_phys_addr = -1UL;
+
+#if defined(CFG_MX7)
+static paddr_t ocram_tz_start_addr = -1UL;
+
+paddr_t imx_get_ocram_tz_start_addr(void)
+{
+	if (ocram_tz_start_addr != -1UL)
+		return ocram_tz_start_addr;
+	else
+		return TRUSTZONE_OCRAM_START;
+}
+
+#ifdef CFG_DT
+/*
+ * List of nodes to include in the OCRAM TZ space.
+ * The lowest node in the OCRAM will define the ocram_tz_start_addr.
+ * The lowest node must have a 4k aligned address.
+ * If this list is empty, imx_get_ocram_tz_start_addr() will return the
+ * default TRUSTZONE_OCRAM_START.
+ */
+static const char * const tz_ocram_match[] = {
+	"fsl,optee-lpm-sram",
+	NULL,
+};
+
+/*
+ * Find the lowest address among nodes listed in tz_ocram_match[]. The lowest
+ * address will define the start address of the Trustzone protected ocram space.
+ * If something goes wrong during dtb parsing, the system panics.
+ * If tz_ocram_match[] is empty, we return -1UL. In that case,
+ * imx_get_ocram_tz_start_addr() will return TRUSTZONE_OCRAM_START by default.
+ */
+static void dt_find_ocram_tz_addr(void)
+{
+	void *fdt;
+	int offset;
+	paddr_t start_addr = -1UL;
+	paddr_t tmp_addr = 0;
+	uint32_t idx = 0;
+
+	/* Get device tree blob */
+	fdt = get_dt();
+	if (fdt == NULL)
+		panic("No DTB found");
+
+	while (tz_ocram_match[idx] != NULL) {
+		/* Get node */
+		offset = fdt_node_offset_by_compatible(fdt, 0,
+							tz_ocram_match[idx]);
+		if (offset < 0) {
+			EMSG("Cannot find %s node in the device tree",
+							tz_ocram_match[idx]);
+			panic();
+		}
+
+		/* Get address in "reg" property */
+		tmp_addr = _fdt_reg_base_address(fdt, offset);
+		if (!tmp_addr) {
+			EMSG("Cannot get reg property of %s",
+							tz_ocram_match[idx]);
+			panic();
+		}
+
+		/* Addresses must be 4Kbytes aligned to be TZ*/
+		if (tmp_addr & 0xFFF) {
+			EMSG("%s address is not 4Kbytes aligned",
+							tz_ocram_match[idx]);
+			panic();
+		}
+
+		/*
+		 * Get the lowest address among nodes to protect listed in
+		 * tz_ocram_match[].
+		 */
+		if ((tmp_addr < start_addr) || (start_addr == -1UL))
+			start_addr = tmp_addr;
+
+		idx++;
+	}
+	ocram_tz_start_addr = start_addr;
+}
+#endif /* CFG_DT */
+#endif /* CFG_MX7 */
 
 #ifdef CFG_MX7
 static const paddr_t phys_addr_imx7[] = {
@@ -24,8 +111,16 @@ static const paddr_t phys_addr_imx7[] = {
 #if defined(CFG_MX7)
 static void init_tz_ocram(void)
 {
-	vaddr_t  iomux_base = 0;
-	uint32_t val = 0;
+	/* Configure the Secure OCRAM granularity */
+	vaddr_t  iomux_base;
+	uint32_t val;
+	uint32_t lock;
+	uint32_t lock_val;
+
+#ifdef CFG_DT
+	/* Get low tz ocram address */
+	dt_find_ocram_tz_addr();
+#endif
 
 	iomux_base = (vaddr_t)phys_to_virt(IOMUXC_GPR_BASE, MEM_AREA_IO_SEC);
 
@@ -33,18 +128,34 @@ static void init_tz_ocram(void)
 
 	/* Configure the OCRAM Retention to start at offset 0 */
 	val &= ~BM_IOMUX_GPR_OCRAM_S_TZ_ADDR;
-	val |= (((TRUSTZONE_OCRAM_START >> 12)
-		 << BP_IOMUX_GPR_OCRAM_S_TZ_ADDR) &
-		BM_IOMUX_GPR_OCRAM_S_TZ_ADDR);
+	// Address is 4 Kbytes granularity
+	val |= (((imx_get_ocram_tz_start_addr() >> 12) <<
+			BP_IOMUX_GPR_OCRAM_S_TZ_ADDR) &
+			BM_IOMUX_GPR_OCRAM_S_TZ_ADDR);
 	val |= IOMUX_GPR_OCRAM_S_TZ_ENABLE;
 
+	lock = BM_IOMUX_GPR_OCRAM_S_TZ_ADDR | IOMUX_GPR_OCRAM_S_TZ_ENABLE;
+
+	/* Check if GPR registers for OCRAM TZ protection are locked */
+	if (IOMUX_GPR_OCRAM_LOCK(lock) & val) {
+		EMSG("GPR Registers for OCRAM TZ Configuration locked");
+		panic();
+	}
+
 	io_write32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID), val);
 
-	/* Lock OCRAM configuration */
-	val = io_read32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID));
-	val |= IOMUX_GPR_OCRAM_LOCK(BM_IOMUX_GPR_OCRAM_S_TZ_ADDR |
-				    IOMUX_GPR_OCRAM_S_TZ_ENABLE);
-	io_write32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID), val);
+	/* Then lock configuration */
+	io_write32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID),
+				IOMUX_GPR_OCRAM_LOCK(lock) | val);
+
+	/*
+	 * Ensure that GPR registers for OCRAM TZ protection locked
+	 * match with the current configuration.
+	 */
+	lock_val =
+		io_read32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID));
+	if ((lock_val & lock) != (val & lock))
+		panic("OCRAM TZ Configuration Lock Mismatch");
 }
 
 static TEE_Result init_ocram(void)
@@ -63,8 +174,14 @@ static TEE_Result init_ocram(void)
 	/* Initialize the Secure OCRAM */
 	init_tz_ocram();
 
+#ifdef CFG_DT
+	/* Move ocram nodes to the OCRAM for Linux */
+	dt_overwrite("fsl,optee-lpm-sram", "reg", "overw_reg", 3);
+	dt_overwrite("fsl,optee-lpm-sram", "clocks", "overw_clock", 3);
+#endif
+
 #ifdef CFG_MX7
-	iram_tlb_phys_addr = TRUSTZONE_OCRAM_START + IRAM_TBL_OFFSET;
+	iram_tlb_phys_addr = imx_get_ocram_tz_start_addr() + IRAM_TBL_OFFSET;
 	phys_addr = phys_addr_imx7;
 	size_area = AIPS1_SIZE; /* 4M for AIPS1/2/3 */
 #endif

quaresmajose/optee_os

(cherry picked from commit b98523d217089daf24569dcf8e335bd2b7478071)

core/arch/arm/plat-imx/imx_dt.c
@@ -1,0 +1,146 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018 NXP
+ *
+ */
+#include <trace.h>
+#include <libfdt.h>
+#include <kernel/boot.h>
+#include <imx.h>
+
+#define DT_U32_PROP_SIZE	4
+#define DT_NUM_PROP_MAX		5
+
+/*
+ * dt_overwrite() overwrites specified properties in the device tree.
+ * Properties to overwrite and new properties are also specified in
+ * the device tree.
+ *
+ * The format is the following :
+ *
+ * overw_str = <&node_1 prop_1 ... prop_n>,
+ *		<&node_2 prop_1 ... prop_n>,
+ *		...
+ *		<&node_n prop_1 ... prop_n>;
+ *
+ * prop_n variables are 32bit integers.
+ *
+ * @node_str: Compatible string of optee node.
+ * @prop_str: Property to overwrite (reg, clocks, ...).
+ * @overw_str: Array in node_str containing new values to overwrite property
+ *		specified prop_str.
+ * @size_prop: Size of the property to overwrite.
+ */
+void dt_overwrite(const char *node_str, const char *prop_str,
+				const char *overw_str, uint32_t size_prop)
+{
+	void *fdt, *prop;
+	uint32_t *fdt_overw_node;
+	int offset, phandle, ret, len;
+	uint32_t i, j, sub_node, sub[DT_NUM_PROP_MAX];
+
+	if (size_prop > DT_NUM_PROP_MAX)
+		EMSG("Number of properties to overwrite is too high");
+
+	if (node_str == NULL) {
+		EMSG("Compatible string empty");
+		return;
+	}
+
+	/* Get dtb */
+	fdt = get_dt();
+	if (fdt == NULL) {
+		EMSG("No DTB found");
+		return;
+	}
+
+	/* Get offset of node_str */
+	offset = fdt_node_offset_by_compatible(fdt, 0, node_str);
+	if (offset < 0) {
+		EMSG("Cannot find %s node in the device tree", node_str);
+		return;
+	}
+
+	/* Get overw_node property */
+	fdt_overw_node = (uint32_t *)fdt_getprop(fdt, offset, overw_str, &len);
+	if (!fdt_overw_node) {
+		DMSG("%s not found in %s", overw_str, node_str);
+		return;
+	}
+	sub_node = len / (size_prop * DT_U32_PROP_SIZE);
+
+	/* For each node to substitute */
+	for (i = 0; i < sub_node; i++) {
+
+		/* fdt_overw_node[0] has to be the phandle */
+		phandle = fdt32_to_cpu(fdt_overw_node[i*size_prop]);
+
+		/* Save new properties starting from fdt_overw_node[1] */
+		for (j = 0; j < size_prop; j++)
+			sub[j] = fdt_overw_node[i*size_prop+j+1];
+
+		/* Get offset based on the phandle */
+		offset = fdt_node_offset_by_phandle(fdt, phandle);
+		if (offset < 0) {
+			EMSG("Cannot find offset based on phandle");
+			return;
+		}
+
+		/* Get size of prop_str */
+		prop = fdt_getprop_w(fdt, offset, prop_str, &len);
+		if (!prop) {
+			DMSG("There is no property %s in the phandle %d",
+							prop_str, phandle);
+			return;
+		}
+
+		/* Replace prop_str property */
+		ret = fdt_setprop_inplace(fdt, offset, prop_str, sub, len);
+		if (ret) {
+			EMSG("Error setprop inplace ret=%d", ret);
+			return;
+		}
+	}
+}
+
+void dt_debug(const char *node_str, const char *prop)
+{
+	void *fdt;
+	uint32_t *fdt_prop;
+	int offset;
+
+	if (node_str == NULL) {
+		EMSG("Compatible string empty");
+		return;
+	}
+
+	/* Get dtb */
+	fdt = get_dt();
+	if (fdt == NULL) {
+		EMSG("No DTB found");
+		return;
+	}
+
+	/* Get offset of node_str */
+	offset = fdt_node_offset_by_compatible(fdt, 0, node_str);
+	if (offset < 0) {
+		EMSG("Cannot find %s node in the device tree", node_str);
+		return;
+	}
+
+	/* Get overw_clock property */
+	fdt_prop = (uint32_t *)fdt_getprop(fdt, offset, prop, NULL);
+	if (!fdt_prop) {
+		EMSG("%s not found in %s", prop, node_str);
+		return;
+	}
+
+	DMSG(
+		"Node: %s prop %s = 0x%X, 0x%X, 0x%X",
+			node_str,
+			prop,
+			fdt32_to_cpu(fdt_prop[0]),
+			fdt32_to_cpu(fdt_prop[1]),
+			fdt32_to_cpu(fdt_prop[2])
+		);
+}

core/arch/arm/plat-imx/imx_ocram.c
@@ -9,11 +9,98 @@
 #include <mm/core_memprot.h>
 #include <string.h>
 #include <io.h>
-
 #include <imx.h>
 #include <imx_pm.h>
+#ifdef CFG_DT
+#include <libfdt.h>
+#include <kernel/boot.h>
+#include <kernel/dt.h>
+#endif /* CFG_DT */
 
 paddr_t iram_tlb_phys_addr = -1UL;
+
+#if defined(CFG_MX7)
+static paddr_t ocram_tz_start_addr = -1UL;
+
+paddr_t imx_get_ocram_tz_start_addr(void)
+{
+	if (ocram_tz_start_addr != -1UL)
+		return ocram_tz_start_addr;
+	else
+		return TRUSTZONE_OCRAM_START;
+}
+
+#ifdef CFG_DT
+/*
+ * List of nodes to include in the OCRAM TZ space.
+ * The lowest node in the OCRAM will define the ocram_tz_start_addr.
+ * The lowest node must have a 4k aligned address.
+ * If this list is empty, imx_get_ocram_tz_start_addr() will return the
+ * default TRUSTZONE_OCRAM_START.
+ */
+static const char * const tz_ocram_match[] = {
+	"fsl,optee-lpm-sram",
+	NULL,
+};
+
+/*
+ * Find the lowest address among nodes listed in tz_ocram_match[]. The lowest
+ * address will define the start address of the Trustzone protected ocram space.
+ * If something goes wrong during dtb parsing, the system panics.
+ * If tz_ocram_match[] is empty, we return -1UL. In that case,
+ * imx_get_ocram_tz_start_addr() will return TRUSTZONE_OCRAM_START by default.
+ */
+static void dt_find_ocram_tz_addr(void)
+{
+	void *fdt;
+	int offset;
+	paddr_t start_addr = -1UL;
+	paddr_t tmp_addr = 0;
+	uint32_t idx = 0;
+
+	/* Get device tree blob */
+	fdt = get_dt();
+	if (fdt == NULL)
+		panic("No DTB found");
+
+	while (tz_ocram_match[idx] != NULL) {
+		/* Get node */
+		offset = fdt_node_offset_by_compatible(fdt, 0,
+							tz_ocram_match[idx]);
+		if (offset < 0) {
+			EMSG("Cannot find %s node in the device tree",
+							tz_ocram_match[idx]);
+			panic();
+		}
+
+		/* Get address in "reg" property */
+		tmp_addr = _fdt_reg_base_address(fdt, offset);
+		if (!tmp_addr) {
+			EMSG("Cannot get reg property of %s",
+							tz_ocram_match[idx]);
+			panic();
+		}
+
+		/* Addresses must be 4Kbytes aligned to be TZ*/
+		if (tmp_addr & 0xFFF) {
+			EMSG("%s address is not 4Kbytes aligned",
+							tz_ocram_match[idx]);
+			panic();
+		}
+
+		/*
+		 * Get the lowest address among nodes to protect listed in
+		 * tz_ocram_match[].
+		 */
+		if ((tmp_addr < start_addr) || (start_addr == -1UL))
+			start_addr = tmp_addr;
+
+		idx++;
+	}
+	ocram_tz_start_addr = start_addr;
+}
+#endif /* CFG_DT */
+#endif /* CFG_MX7 */
 
 #ifdef CFG_MX7
 static const paddr_t phys_addr_imx7[] = {
@@ -24,8 +111,16 @@ static const paddr_t phys_addr_imx7[] = {
 #if defined(CFG_MX7)
 static void init_tz_ocram(void)
 {
-	vaddr_t  iomux_base = 0;
-	uint32_t val = 0;
+	/* Configure the Secure OCRAM granularity */
+	vaddr_t  iomux_base;
+	uint32_t val;
+	uint32_t lock;
+	uint32_t lock_val;
+
+#ifdef CFG_DT
+	/* Get low tz ocram address */
+	dt_find_ocram_tz_addr();
+#endif
 
 	iomux_base = (vaddr_t)phys_to_virt(IOMUXC_GPR_BASE, MEM_AREA_IO_SEC,
 					   IOMUXC_SIZE);
@@ -34,18 +129,34 @@ static void init_tz_ocram(void)
 
 	/* Configure the OCRAM Retention to start at offset 0 */
 	val &= ~BM_IOMUX_GPR_OCRAM_S_TZ_ADDR;
-	val |= (((TRUSTZONE_OCRAM_START >> 12)
-		 << BP_IOMUX_GPR_OCRAM_S_TZ_ADDR) &
-		BM_IOMUX_GPR_OCRAM_S_TZ_ADDR);
+	// Address is 4 Kbytes granularity
+	val |= (((imx_get_ocram_tz_start_addr() >> 12) <<
+			BP_IOMUX_GPR_OCRAM_S_TZ_ADDR) &
+			BM_IOMUX_GPR_OCRAM_S_TZ_ADDR);
 	val |= IOMUX_GPR_OCRAM_S_TZ_ENABLE;
 
+	lock = BM_IOMUX_GPR_OCRAM_S_TZ_ADDR | IOMUX_GPR_OCRAM_S_TZ_ENABLE;
+
+	/* Check if GPR registers for OCRAM TZ protection are locked */
+	if (IOMUX_GPR_OCRAM_LOCK(lock) & val) {
+		EMSG("GPR Registers for OCRAM TZ Configuration locked");
+		panic();
+	}
+
 	io_write32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID), val);
 
-	/* Lock OCRAM configuration */
-	val = io_read32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID));
-	val |= IOMUX_GPR_OCRAM_LOCK(BM_IOMUX_GPR_OCRAM_S_TZ_ADDR |
-				    IOMUX_GPR_OCRAM_S_TZ_ENABLE);
-	io_write32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID), val);
+	/* Then lock configuration */
+	io_write32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID),
+				IOMUX_GPR_OCRAM_LOCK(lock) | val);
+
+	/*
+	 * Ensure that GPR registers for OCRAM TZ protection locked
+	 * match with the current configuration.
+	 */
+	lock_val =
+		io_read32(iomux_base + IOMUX_GPRx_OFFSET(IOMUX_GPR_OCRAM_ID));
+	if ((lock_val & lock) != (val & lock))
+		panic("OCRAM TZ Configuration Lock Mismatch");
 }
 
 static TEE_Result init_ocram(void)
@@ -64,8 +175,14 @@ static TEE_Result init_ocram(void)
 	/* Initialize the Secure OCRAM */
 	init_tz_ocram();
 
+#ifdef CFG_DT
+	/* Move ocram nodes to the OCRAM for Linux */
+	dt_overwrite("fsl,optee-lpm-sram", "reg", "overw_reg", 3);
+	dt_overwrite("fsl,optee-lpm-sram", "clocks", "overw_clock", 3);
+#endif
+
 #ifdef CFG_MX7
-	iram_tlb_phys_addr = TRUSTZONE_OCRAM_START + IRAM_TBL_OFFSET;
+	iram_tlb_phys_addr = imx_get_ocram_tz_start_addr() + IRAM_TBL_OFFSET;
 	phys_addr = phys_addr_imx7;
 	size_area = AIPS1_SIZE; /* 4M for AIPS1/2/3 */
 #endif