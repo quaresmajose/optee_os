nxp-imx/imx-optee-os

TEE-272 Cortex-A9 add PL310 Linux/Optee Mutex

core/arch/arm/plat-imx/imx_pl310.c
@@ -23,6 +23,7 @@
 #define PL310_DEBUG_CTRL_DISABLE_LINEFILL	BIT(0)
 
 register_phys_mem(MEM_AREA_IO_SEC, PL310_BASE, CORE_MMU_DEVICE_SIZE);
+register_phys_mem(MEM_AREA_IO_NSEC, PL310_BASE, CORE_MMU_DEVICE_SIZE);
 
 void arm_cl2_config(vaddr_t pl310_base)
 {
@@ -83,6 +84,11 @@ vaddr_t pl310_base(void)
 	return core_mmu_get_va(PL310_BASE, MEM_AREA_IO_SEC);
 }
 
+vaddr_t pl310_nsbase(void)
+{
+	return core_mmu_get_va(PL310_BASE, MEM_AREA_IO_NSEC);
+}
+
 #ifdef CFG_PL310_SIP_PROTOCOL
 uint32_t pl310_enable(void)
 {
@@ -123,3 +129,4 @@ uint32_t pl310_enable_wflz(void)
 	return OPTEE_SMC_RETURN_OK;
 }
 #endif
+

quaresmajose/optee_os

Check if the cache area to maintain is secure or non-secure.
Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
(cherry picked from commit ff67b9f6d071283b11facbda414af12525c303b3)

core/arch/arm/plat-imx/imx_pl310.c
@@ -24,6 +24,7 @@
 #define PL310_PREFETCH_DOUBLE_LINEFILL		BIT(30)
 
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, PL310_BASE, CORE_MMU_PGDIR_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_NSEC, PL310_BASE, CORE_MMU_PGDIR_SIZE);
 
 void arm_cl2_config(vaddr_t pl310_base)
 {
@@ -86,7 +87,12 @@ void arm_cl2_enable(vaddr_t pl310_base)
 
 vaddr_t pl310_base(void)
 {
-	return core_mmu_get_va(PL310_BASE, MEM_AREA_IO_SEC, 1);
+	return core_mmu_get_va(PL310_BASE, MEM_AREA_IO_SEC, PL310_SIZE);
+}
+
+vaddr_t pl310_nsbase(void)
+{
+	return core_mmu_get_va(PL310_BASE, MEM_AREA_IO_NSEC, PL310_SIZE);
 }
 
 #ifdef CFG_PL310_SIP_PROTOCOL