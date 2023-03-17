nxp-imx/imx-optee-os

TEE-335 Implementation busfreq i.MX6
   Add busfreq functionality for i.MX6
    - Enable and Verification:
       6QP/6Q
       6UL
       6DL

    - 6SX (disabled) => not functional

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/arch/arm/plat-imx/busfreq/busfreq.c
@@ -1,0 +1,117 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/**
+ * @copyright 2018 NXP
+ *
+ * @file    busfreq.c
+ *
+ * @brief   Implementation of the bus frequency scaling.\n
+ *          Interface and initialization.
+ */
+
+/* Global includes */
+#include <initcall.h>
+#include <kernel/generic_boot.h>
+#include <libfdt.h>
+#include <tee_api_types.h>
+#include <trace.h>
+
+/* Platform includes */
+#include <imx.h>
+#include <imx_busfreq.h>
+
+/* Local include */
+#include "busfreq_imx6.h"
+
+#ifndef CFG_IMX6
+__weak TEE_Result imx6_busfreq_change(uint32_t freq __unused,
+		uint32_t dll_off __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+
+__weak TEE_Result imx6_busfreq_init(void)
+{
+	return TEE_ERROR_GENERIC;
+}
+#endif
+
+/**
+ * @brief   Change the bus frequency on any i.MX devices
+ *          to the requested frequency \a freq and
+ *          switch DLL off is \a dll_off = 1
+ *
+ * @param[in] freq     Frequency to switch to
+ * @param[in] dll_off  switch DLL off or not
+ *
+ * @retval  TEE_SUCCESS              No error
+ * @retvql  TEE_ERROR_NOT_SUPPORTED  Feature not supported
+ */
+TEE_Result busfreq_change(uint32_t freq, uint32_t dll_off)
+{
+	TEE_Result ret = TEE_ERROR_NOT_SUPPORTED;
+
+	if (soc_is_imx6())
+		ret = imx6_busfreq_change(freq, dll_off);
+
+	return ret;
+}
+
+#ifdef CFG_DT
+/**
+ * @brief   Add the busfreq property into the "/firmware/optee"
+ *          node.
+ *
+ * @retval 0    if success
+ * @retval (-1) otherwise
+ */
+static int dt_busfreq(void)
+{
+	void *fdt = get_dt_blob();
+	int  offs;
+	int  ret;
+
+	offs = fdt_path_offset(fdt, "/firmware/optee");
+
+	if (offs < 0) {
+		EMSG("OP-TEE Device Tree doesn't exist!\n");
+		return (-1);
+	}
+
+	ret = fdt_setprop_u32(fdt, offs, "busfreq", 1);
+	if (ret < 0)
+		return (-1);
+
+	return 0;
+}
+#else
+static int dt_busfreq(void)
+{
+	return 0;
+}
+#endif
+/**
+ * @brief   Allocation and initialization of the OCRAM
+ *          into which memory frequency change function
+ *          will be executed
+ *
+ * @retval  TEE_SUCCESS         Success
+ * @retval  TEE_ERROR_GENERIC   Generic Error
+ */
+static TEE_Result busfreq_init(void)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+
+	if (soc_is_imx6())
+		ret = imx6_busfreq_init();
+
+	if (ret == TEE_SUCCESS) {
+		if (dt_busfreq()) {
+			EMSG("Failed to config Bus Frequency");
+			ret = TEE_ERROR_GENERIC;
+		}
+	}
+
+	return ret;
+}
+
+service_init(busfreq_init);

core/arch/arm/plat-imx/busfreq/busfreq_imx6.h
@@ -1,0 +1,50 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/**
+ * @copyright 2018 NXP
+ *
+ * @file    busfreq_imx6.h
+ *
+ * @brief   Busfreq imx6 header.
+ */
+#ifndef __BUSFREQ_IMX6_H__
+#define __BUSFREQ_IMX6_H__
+
+#ifndef ASM
+/**
+ * @brief   Definition of the structure used during the bus frequency
+ *          operation.
+ *          This structure must be inline with the assembly code offset
+ *          definition.
+ */
+struct busfreq_info {
+	uint32_t cpu_type;
+	uint32_t freq;
+	uint32_t dll_off;
+	uint32_t (*ddr_settings)[2];
+	uint32_t ddr_settings_size;
+	uint32_t (*ddr_calibration)[2];
+	uint32_t ddr_calibration_size;
+	uint32_t (*iomux_offsets)[2];
+	uint32_t iomux_offsets_size;
+	uint32_t ccm_base;
+	uint32_t iomux_base;
+	uint32_t mmdc_base;
+	uint32_t pl310_v_base;
+} __aligned(8);
+
+TEE_Result imx6_busfreq_init(void);
+TEE_Result imx6_busfreq_change(uint32_t freq, uint32_t dll_off);
+
+void imx6_ddr3_freq_change(struct busfreq_info *info);
+size_t get_imx6_ddr3_freq_change_size(void);
+void imx6_lpddr2_freq_change(struct busfreq_info *info);
+size_t get_imx6_lpddr2_freq_change_size(void);
+#endif
+/*
+ * Define the CPU ID bit mask needed in the Assembly code
+ */
+#define BUSFREQ_CPU_MX6UL		BIT32(0)
+#define BUSFREQ_CPU_MX6QP		BIT32(1)
+#define BUSFREQ_CPU_MX6SX		BIT32(2)
+
+#endif

quaresmajose/optee_os

    - i.MX6SX Busfreq support:
       * optimization of the i.MX6SX suspend/idle to reduce space
         used in OCRAM_S (data/function install at the beginning
         of the OCRAM_S)
       * install the busfreq function at the end of the OCRAM_S
(cherry picked from commit 75921d49cf3a9496c8feba3cb1c33fdf8918d20c)

core/arch/arm/plat-imx/busfreq/busfreq.c
@@ -1,0 +1,117 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/**
+ * @copyright 2018 NXP
+ *
+ * @file    busfreq.c
+ *
+ * @brief   Implementation of the bus frequency scaling.\n
+ *          Interface and initialization.
+ */
+
+/* Global includes */
+#include <initcall.h>
+#include <kernel/boot.h>
+#include <libfdt.h>
+#include <tee_api_types.h>
+#include <trace.h>
+
+/* Platform includes */
+#include <imx.h>
+#include <imx_busfreq.h>
+
+/* Local include */
+#include "busfreq_imx6.h"
+
+#ifndef CFG_IMX6
+__weak TEE_Result imx6_busfreq_change(uint32_t freq __unused,
+		uint32_t dll_off __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+
+__weak TEE_Result imx6_busfreq_init(void)
+{
+	return TEE_ERROR_GENERIC;
+}
+#endif
+
+/**
+ * @brief   Change the bus frequency on any i.MX devices
+ *          to the requested frequency \a freq and
+ *          switch DLL off is \a dll_off = 1
+ *
+ * @param[in] freq     Frequency to switch to
+ * @param[in] dll_off  switch DLL off or not
+ *
+ * @retval  TEE_SUCCESS              No error
+ * @retvql  TEE_ERROR_NOT_SUPPORTED  Feature not supported
+ */
+TEE_Result busfreq_change(uint32_t freq, uint32_t dll_off)
+{
+	TEE_Result ret = TEE_ERROR_NOT_SUPPORTED;
+
+	if (soc_is_imx6())
+		ret = imx6_busfreq_change(freq, dll_off);
+
+	return ret;
+}
+
+#ifdef CFG_DT
+/**
+ * @brief   Add the busfreq property into the "/firmware/optee"
+ *          node.
+ *
+ * @retval 0    if success
+ * @retval (-1) otherwise
+ */
+static int dt_busfreq(void)
+{
+	void *fdt = get_dt();
+	int  offs;
+	int  ret;
+
+	offs = fdt_path_offset(fdt, "/firmware/optee");
+
+	if (offs < 0) {
+		EMSG("OP-TEE Device Tree doesn't exist!\n");
+		return (-1);
+	}
+
+	ret = fdt_setprop_u32(fdt, offs, "busfreq", 1);
+	if (ret < 0)
+		return (-1);
+
+	return 0;
+}
+#else
+static int dt_busfreq(void)
+{
+	return 0;
+}
+#endif
+/**
+ * @brief   Allocation and initialization of the OCRAM
+ *          into which memory frequency change function
+ *          will be executed
+ *
+ * @retval  TEE_SUCCESS         Success
+ * @retval  TEE_ERROR_GENERIC   Generic Error
+ */
+static TEE_Result busfreq_init(void)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+
+	if (soc_is_imx6())
+		ret = imx6_busfreq_init();
+
+	if (ret == TEE_SUCCESS) {
+		if (dt_busfreq()) {
+			EMSG("Failed to config Bus Frequency");
+			ret = TEE_ERROR_GENERIC;
+		}
+	}
+
+	return ret;
+}
+
+service_init(busfreq_init);

core/arch/arm/plat-imx/busfreq/busfreq_imx6.h
@@ -1,0 +1,51 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/**
+ * @copyright 2018 NXP
+ *
+ * @file    busfreq_imx6.h
+ *
+ * @brief   Busfreq imx6 header.
+ */
+#ifndef __BUSFREQ_IMX6_H__
+#define __BUSFREQ_IMX6_H__
+
+#ifndef __ASSEMBLER__
+/**
+ * @brief   Definition of the structure used during the bus frequency
+ *          operation.
+ *          This structure must be inline with the assembly code offset
+ *          definition.
+ */
+struct busfreq_info {
+	uint32_t cpu_type;
+	uint32_t freq;
+	uint32_t dll_off;
+	uint32_t (*ddr_settings)[2];
+	uint32_t ddr_settings_size;
+	uint32_t (*ddr_calibration)[2];
+	uint32_t ddr_calibration_size;
+	uint32_t (*iomux_offsets)[2];
+	uint32_t iomux_offsets_size;
+	uint32_t ccm_base;
+	uint32_t iomux_base;
+	uint32_t mmdc_base;
+	uint32_t pl310_v_base;
+	uint32_t ttbr1;
+} __aligned(8);
+
+TEE_Result imx6_busfreq_init(void);
+TEE_Result imx6_busfreq_change(uint32_t freq, uint32_t dll_off);
+
+void imx6_ddr3_freq_change(struct busfreq_info *info);
+size_t get_imx6_ddr3_freq_change_size(void);
+void imx6_lpddr2_freq_change(struct busfreq_info *info);
+size_t get_imx6_lpddr2_freq_change_size(void);
+#endif
+/*
+ * Define the CPU ID bit mask needed in the Assembly code
+ */
+#define BUSFREQ_CPU_MX6UL		BIT32(0)
+#define BUSFREQ_CPU_MX6QP		BIT32(1)
+#define BUSFREQ_CPU_MX6SX		BIT32(2)
+
+#endif