nxp-imx/imx-optee-os

TEE-409 core: imx: re-init TZASC after suspend on imx8m
TZASC was not configured properly after resuming from suspend.
The tzasc is now re-initialized using PM callback.

Signed-off-by: Alessandro Di Chiara <alessandro.dichiara@nxp.com>
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/arch/arm/plat-imx/pm/pm-imx8.c
@@ -1,0 +1,71 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2019 NXP
+ *
+ */
+
+#include <kernel/pm.h>
+#include <imx.h>
+#include <imx_pm.h>
+#include <trace.h>
+#include <util.h>
+
+/**
+ * @brief   Handler for cpu resume
+ *
+ * @param[in] a0   Max power level powered down
+ * @param[in] a1   Unusued
+ *
+ * @retval TEE_SUCCESS         Success
+ * @retval TEE_ERROR_GENERIC   Generic error
+ */
+unsigned long cpu_resume_handler(unsigned long a0,
+				 unsigned long a1 __unused)
+{
+	TEE_Result retstatus = TEE_SUCCESS;
+
+	/* a0 is the highest power level which was powered down.
+	 * This can be:
+	 *	PM_CORE_LEVEL i.e. "0" in case of Core level
+	 *	PM_CLUSTER_LEVEL i.e. "1" in case of Cluster level
+	 *	PM_SYSTEM_LEVEL "2" in case of System level (Not tested)
+	 */
+	if (a0 == PM_CLUSTER_LEVEL)
+		retstatus = pm_change_state(PM_OP_RESUME,
+					PM_HINT_CONTEXT_STATE);
+
+	/* Returned value to the ATF.
+	 * If it is not 0, the system will panic
+	 */
+	return retstatus;
+}
+
+/**
+ * @brief   Handler for cpu suspend
+ *
+ * @param[in] a0   Max power level to power down
+ * @param[in] a1   Unusued
+ *
+ * @retval TEE_SUCCESS         Success
+ * @retval TEE_ERROR_GENERIC   Generic error
+ */
+unsigned long cpu_suspend_handler(unsigned long a0,
+				 unsigned long a1 __unused)
+{
+	TEE_Result retstatus = TEE_SUCCESS;
+
+	/* a0 is the highest power level which was powered down.
+	 * This can be:
+	 *	PM_CORE_LEVEL i.e. "0" in case of Core level
+	 *	PM_CLUSTER_LEVEL i.e. "1" in case of Cluster level
+	 *	PM_SYSTEM_LEVEL "2" in case of System level (Not tested)
+	 */
+	if (a0 == PM_CLUSTER_LEVEL)
+		retstatus = pm_change_state(PM_OP_SUSPEND,
+					PM_HINT_CONTEXT_STATE);
+
+	/* Returned value to the ATF.
+	 * If it is not 0, the system will panic
+	 */
+	return retstatus;
+}

core/arch/arm/plat-imx/pm/sub.mk
@@ -1,5 +1,5 @@
 global-incdirs-y += .
-srcs-y += psci.c
+srcs-$(CFG_PSCI_ARM32) += psci.c
 srcs-$(CFG_MX7) += pm-imx7.c psci-suspend-imx7.S imx7_suspend.c \
 	cpuidle-imx7d.c psci-cpuidle-imx7.S gpcv2.c
 
@@ -10,5 +10,6 @@ srcs-$(CFG_MX6SX) += psci-cpuidle-imx6sx.S cpuidle-imx6sx.c
 srcs-$(CFG_MX6SL) += psci-cpuidle-imx6sl.S cpuidle-imx6sl.c
 srcs-$(CFG_MX6SLL) += psci-cpuidle-imx6sll.S cpuidle-imx6sll.c
 srcs-$(CFG_MX7ULP) += pm-imx7ulp.c psci-suspend-imx7ulp.S imx7ulp_suspend.c
+srcs-$(CFG_MX8MM) += pm-imx8.c
 
 cflags-psci.c-y += -Wno-suggest-attribute=noreturn

quaresmajose/optee_os

(cherry picked from commit 591d44f425d2a71b2623d185554713b051834b6e)

core/arch/arm/plat-imx/pm/pm-imx8.c
@@ -1,0 +1,71 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2019 NXP
+ *
+ */
+
+#include <kernel/pm.h>
+#include <kernel/thread.h>
+#include <imx_pm.h>
+#include <trace.h>
+#include <util.h>
+
+/**
+ * @brief   Handler for cpu resume
+ *
+ * @param[in] a0   Max power level powered down
+ * @param[in] a1   Unusued
+ *
+ * @retval TEE_SUCCESS         Success
+ * @retval TEE_ERROR_GENERIC   Generic error
+ */
+unsigned long thread_cpu_resume_handler(unsigned long a0,
+						unsigned long a1 __unused)
+{
+	TEE_Result retstatus = TEE_SUCCESS;
+
+	/* a0 is the highest power level which was powered down.
+	 * This can be:
+	 *	PM_CORE_LEVEL i.e. "0" in case of Core level
+	 *	PM_CLUSTER_LEVEL i.e. "1" in case of Cluster level
+	 *	PM_SYSTEM_LEVEL "2" in case of System level (Not tested)
+	 */
+	if (a0 == PM_CLUSTER_LEVEL)
+		retstatus = pm_change_state(PM_OP_RESUME,
+					PM_HINT_CONTEXT_STATE);
+
+	/* Returned value to the ATF.
+	 * If it is not 0, the system will panic
+	 */
+	return retstatus;
+}
+
+/**
+ * @brief   Handler for cpu suspend
+ *
+ * @param[in] a0   Max power level to power down
+ * @param[in] a1   Unusued
+ *
+ * @retval TEE_SUCCESS         Success
+ * @retval TEE_ERROR_GENERIC   Generic error
+ */
+unsigned long thread_cpu_suspend_handler(unsigned long a0,
+						unsigned long a1 __unused)
+{
+	TEE_Result retstatus = TEE_SUCCESS;
+
+	/* a0 is the highest power level which was powered down.
+	 * This can be:
+	 *	PM_CORE_LEVEL i.e. "0" in case of Core level
+	 *	PM_CLUSTER_LEVEL i.e. "1" in case of Cluster level
+	 *	PM_SYSTEM_LEVEL "2" in case of System level (Not tested)
+	 */
+	if (a0 == PM_CLUSTER_LEVEL)
+		retstatus = pm_change_state(PM_OP_SUSPEND,
+					PM_HINT_CONTEXT_STATE);
+
+	/* Returned value to the ATF.
+	 * If it is not 0, the system will panic
+	 */
+	return retstatus;
+}

core/arch/arm/plat-imx/pm/sub.mk
@@ -1,5 +1,5 @@
 global-incdirs-y += .
-srcs-y += psci.c
+srcs-$(CFG_PSCI_ARM32) += psci.c
 srcs-$(CFG_MX7) += pm-imx7.c psci-suspend-imx7.S imx7_suspend.c \
 	cpuidle-imx7d.c psci-cpuidle-imx7.S gpcv2.c
 
@@ -10,5 +10,6 @@ srcs-$(CFG_MX6SX) += psci-cpuidle-imx6sx.S cpuidle-imx6sx.c
 srcs-$(CFG_MX6SL) += psci-cpuidle-imx6sl.S cpuidle-imx6sl.c
 srcs-$(CFG_MX6SLL) += psci-cpuidle-imx6sll.S cpuidle-imx6sll.c
 srcs-$(CFG_MX7ULP) += pm-imx7ulp.c psci-suspend-imx7ulp.S imx7ulp_suspend.c
+srcs-$(CFG_MX8M) += pm-imx8.c
 
 cflags-psci.c-y += -Wno-suggest-attribute=noreturn